18c1bfd371b0caaa0bbc7af9216982db5787fd47
bbbb <zcsw99wrmy@privaterelay.appleid.com>
2026-07-11 18:27:01 +0200

    Update random-airdrop.js

--- api/random-airdrop.js
+++ api/random-airdrop.js
@@ -7,6 +7,15 @@ const ADMIN_TOKEN = process.env.ADMIN_AIRDROP_TOKEN;
 
 export default async function handler(req, res) {
   res.setHeader('Cache-Control', 'no-store');
+  // CORS: разрешаем вызов из браузера (в т.ч. с локальной тестовой страницы
+  // file://), т.к. кастомный заголовок x-admin-token триггерит preflight.
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-admin-token');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end();
+  }
 
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
